a8ee00efb74fcb67f61df6e04cf3a212b78d6821
Anca <barbuanca6@gmail.com>
2024-04-21 09:40:28 +0300

    Display Sudoku Grid

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SudokuSolver.Web;
+using System.Text;
+using System;
+using System.Threading.Tasks;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace SudokuSolver.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            await builder.Build().RunAsync();
+        }
+    }
+}
 
-await builder.Build().RunAsync();
